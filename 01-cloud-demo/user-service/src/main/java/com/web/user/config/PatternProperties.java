b3b1ba5a70f6e039dbdf88ba0464233ae9997665
Eascaty <63639459+Eascaty@users.noreply.github.com>
2021-09-03 16:49:26 +0800

    完成nacos多环节配置共享

---
+++ 01-cloud-demo/user-service/src/main/java/com/web/user/config/PatternProperties.java
@@ -0,0 +1,18 @@
+package com.web.user.config;
+
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+@Data
+@Component
+@ConfigurationProperties(prefix = "pattern")
+public class PatternProperties {
+    private String dateformat;
+    private String envSharedValue;
+
+
+
+}
