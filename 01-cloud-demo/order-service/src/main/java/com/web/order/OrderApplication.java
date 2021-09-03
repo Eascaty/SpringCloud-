4c7d5fc67e66b7805ecaabcf21318bd968ae2f5e
Eascaty <63639459+Eascaty@users.noreply.github.com>
2021-09-03 14:38:48 +0800

    完成Ribbon负载均衡

--- 01-cloud-demo/order-service/src/main/java/com/web/order/OrderApplication.java
+++ 01-cloud-demo/order-service/src/main/java/com/web/order/OrderApplication.java
@@ -1,5 +1,7 @@
 package com.web.order;
 
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RandomRule;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -24,5 +26,11 @@ public class OrderApplication {
     @LoadBalanced
     public RestTemplate restTemplate(){
         return new RestTemplate();
+    }
+
+    //全体Order
+    @Bean
+    public IRule randomRule(){
+        return  new RandomRule();
     }
 }
